5714985f90527c01cfb56ce4c3cff6d7fb48b960
Albert Chang <a89529294@hotmail.com>
2025-04-29 13:46:20 +0800

    add localprod

--- packages/frontend/upload-to-s3.sh
+++ packages/frontend/upload-to-s3.sh
@@ -16,4 +16,7 @@ S3_BUCKET=${S3_BUCKET:-"sample-react-fe"}
 # Upload the build output to S3 (sync for efficiency)
 aws s3 sync ./dist "s3://$S3_BUCKET/" --delete
 
+# Invalidate CloudFront cache
+aws cloudfront create-invalidation --distribution-id E1T1MHZ13LE77Q --paths "/*"
+
 echo "Build and upload to S3 complete!"
