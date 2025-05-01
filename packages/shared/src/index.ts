2bd6a775905983f75e205e862bc495e4d99c8aff
Albert Chang <a89529294@hotmail.com>
2025-05-01 11:44:14 +0800

    before tweaking ts configs

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,32 +1,55 @@
 import { z } from "zod";
+import { CreateInsertSchema, createSelectSchema } from "drizzle-zod";
+import { employeesTable } from "./schema.js";
 
-export const baseEmployeeSelectSchema = z.object({
-  id: z.string().uuid(),
-  idNumber: z.string(),
-  chName: z.string(),
-  enName: z.string().nullable(),
-  birthday: z.date().nullable(),
-  gender: z.enum(["male", "female"]),
-  marital_status: z.string().nullable(),
-  education: z.string().nullable(),
-  phone1: z.string(),
-  email: z.string().email().nullable(),
-  residenceCounty: z.string().nullable(),
-  residenceDistrict: z.string().nullable(),
-  residenceAddress: z.string().nullable(),
-  mailingCounty: z.string().nullable(),
-  mailingDistrict: z.string().nullable(),
-  mailingAddress: z.string().nullable(),
+// export const baseEmployeeSelectSchema = z.object({
+//   id: z.string().uuid(),
+//   idNumber: z.string(),
+//   chName: z.string(),
+//   enName: z.string().nullable(),
+//   birthday: z.date().nullable(),
+//   gender: z.enum(["male", "female"]),
+//   marital_status: z.string().nullable(),
+//   education: z.string().nullable(),
+//   phone1: z.string(),
+//   email: z.string().email().nullable(),
+//   residenceCounty: z.string().nullable(),
+//   residenceDistrict: z.string().nullable(),
+//   residenceAddress: z.string().nullable(),
+//   mailingCounty: z.string().nullable(),
+//   mailingDistrict: z.string().nullable(),
+//   mailingAddress: z.string().nullable(),
+// });
+
+const baseEmployeeSelectSchema1 = createSelectSchema(employeesTable).pick({
+  id: true,
+  idNumber: true,
+  // chName: true,
+  // enName: true,
+  birthday: true,
+  // gender: true,
+  // marital_status: true,
+  // education: true,
+  // phone1: true,
+  // email: true,
+  // residenceCounty: true,
+  // residenceDistrict: true,
+  // residenceAddress: true,
+  // mailingCounty: true,
+  // mailingDistrict: true,
+  // mailingAddress: true,
 });
 
-export const employeeSelectSchema = baseEmployeeSelectSchema.extend({
-  departments: z.array(
-    z.object({
-      departmentName: z.string().min(1),
-      departmentId: z.string().min(1),
-      jobTitle: z.string().min(1),
-    })
-  ),
+export const employeeSelectSchema = baseEmployeeSelectSchema1.extend({
+  //   departments: z.array(
+  //     z.object({
+  //       departmentName: z.string().min(1),
+  //       departmentId: z.string().min(1),
+  //       jobTitle: z.string().min(1),
+  //     })
+  //   ),
 });
 
 export type EmployeeSelect = z.infer<typeof employeeSelectSchema>;
+
+export * from "./schema.js";
